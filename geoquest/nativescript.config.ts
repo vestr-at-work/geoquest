f47f8b3549ee72be8469af1c3dd5cb23ea489df5
vestr-at-work <petera-david@seznam.cz>
2023-10-26 13:27:16 +0200

    Inicialize again and better

--- geoquest/nativescript.config.ts
+++ geoquest/nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.myapp',
+  id: 'org.nativescript.geoquest',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {

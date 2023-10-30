511cbb0af125f1d1c4382b7123cdc9d3e08c7e4a
vestr-at-work <petera-david@seznam.cz>
2023-10-30 15:29:39 +0100

    Add basic navigation

--- geoquest/app/app.ts
+++ geoquest/app/app.ts
@@ -4,7 +4,18 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first page.
 */
 
-import { svelteNativeNoFrame } from 'svelte-native'
-import App from './App.svelte'
+import { svelteNativeNoFrame } from 'svelte-native';
+import { registerNativeViewElement } from 'svelte-native/dom';
+import App from './App.svelte';
+
+import { installMixins } from '@nativescript-community/ui-material-core';
+import { TabContentItem, TabStrip, TabStripItem, Tabs } from '@nativescript-community/ui-material-tabs';
+
+registerNativeViewElement('tabs', () => Tabs);
+registerNativeViewElement('tabStrip', () => TabStrip);
+registerNativeViewElement('tabStripItem', () => TabStripItem);
+registerNativeViewElement('tabContentItem', () => TabContentItem);
+
+installMixins();
 
 svelteNativeNoFrame(App, {})
--- geoquest/app/components/Home.svelte
+++ geoquest/app/components/Home.svelte
@@ -1,4 +1,4 @@
-<page>
+<!-- <page>
     <actionBar title="Home" />
     <gridLayout>
         <label class="info">
@@ -24,4 +24,43 @@
         horizontal-align: center;
         vertical-align: center;
     }
-</style>
+</style> -->
+<page actionBarHidden={true}>
+    <tabs bind:selectedIndex={selectedTab}>
+
+        <!-- The bottom tab UI is created via TabStrip (the containier) and TabStripItem (for each tab)-->
+        <tabStrip>
+            <tabStripItem>
+                <image src="font://&#xf279;" class="far" />
+            </tabStripItem>
+            <tabStripItem>
+                <image src="font://&#xf059;" class="far" />
+            </tabStripItem>
+            <tabStripItem>
+                <image src="font://&#xf007;" class="far" />
+            </tabStripItem>
+        </tabStrip>
+
+        <!-- The number of TabContentItem components should corespond to the number of TabStripItem components -->
+        <tabContentItem>
+            <gridLayout>
+                <label text="Map Page" class="h2 text-center" />
+            </gridLayout>
+        </tabContentItem>
+        <tabContentItem>
+            <gridLayout>
+                <label text="Quests Page" class="h2 text-center" />
+            </gridLayout>
+        </tabContentItem>
+        <tabContentItem>
+            <gridLayout>
+                <label text="Account Page" class="h2 text-center" />
+            </gridLayout>
+        </tabContentItem>
+
+    </tabs>
+</page>
+
+<script lang="ts">
+    let selectedTab: number;
+</script>
